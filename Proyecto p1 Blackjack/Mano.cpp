dccd80482e1c3b47810abe6bff02265a800af30b
Katherine JR <Katherine8jara@gmail.com>
2024-01-27 21:48:53 -0600

    Cambios en la clase Mano

--- Proyecto p1 Blackjack/Mano.cpp
+++ Proyecto p1 Blackjack/Mano.cpp
@@ -1,1 +1,40 @@
 #include "Mano.h"
+Mano::Mano()
+{
+	cartUsadas = 0;
+
+	for (int i = 0; i < 52; i++) {
+		this->cartas[i] = new Carta();
+	}
+}
+
+Mano::~Mano()
+{
+	for (int i = 0; i < 52; i++) {
+		if (cartas[i] != nullptr)
+			delete cartas[i];
+	}
+}
+
+void Mano::agregarCarta(Mazo* nuevaCarta)
+{
+	if (cartUsadas < 52) {
+		cartas[cartUsadas] = nuevaCarta->tomarCarta();
+		cartUsadas++;
+	}
+}
+
+void Mano::limpiar()
+{
+	for (int i = 0; i < cartUsadas; i++) {
+		cartas[i] = nullptr; //podemos usarlo para la siguiente partida, por eso no se elimina el vector
+	}
+}
+
+int Mano::getPuntos()
+{
+	int sumaPuntosCartas = 0;
+	for (int i = 0; i < cartUsadas; i++) {
+		sumaPuntosCartas += cartas[i]->getValor();
+	}
+}
--- Proyecto p1 Blackjack/Mano.h
+++ Proyecto p1 Blackjack/Mano.h
@@ -1,12 +1,21 @@
 #ifndef MANO_H
 #define MANO_H
-
+#include "Mazo.h"
 
 //ESTAMOS SEGURAS DE QUE EL VECTOR DE MANO ES DE 52?
 //PERO SI UN JUGADOR SOLO PUEDE TENER  CARTAS, PARA AHORRAR MEMORIA SERIA MEJOR 
 class Mano{
 
+private:
+	Carta* cartas[52];
+	int cartUsadas;
 
+public:
+	Mano();
+	~Mano();
+	void agregarCarta(Mazo* nuevaCarta);
+	void limpiar(); //aqui se puede cambiar a un bool para ver si se logra eliminar
+	int getPuntos();
 
 
 };
